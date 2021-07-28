e3d846c2399424ed2c4bdaf48ae4cbd9b6e62c29
Gautam Savaliya <gautam.savaliya@newpage.io>
2021-07-28 23:08:34 +0530

    refrenced wrong output variable

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,6 @@ output "cluster_endpoint" {
 }
 
 output "cluster_ca_certificate" {
-  value       = google_container_cluster.primary.master_auth.0.client_certificate
+  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
   description = "string ||| Base64 encoded public certificate used by clients to authenticate to the cluster endpoint."
 }
