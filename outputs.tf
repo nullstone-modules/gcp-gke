516f69168817a48538abb3b6bc8cac5f7501c52e
Gautam Savaliya <gautam.savaliya@newpage.io>
2021-07-30 19:21:05 +0530

    enabled sensitive flag cluster_ca_certificate output

--- outputs.tf
+++ outputs.tf
@@ -11,4 +11,5 @@ output "cluster_endpoint" {
 output "cluster_ca_certificate" {
   value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
   description = "string ||| Base64 encoded public certificate used by clients to authenticate to the cluster endpoint."
+  sensitive = true
 }
