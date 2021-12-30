e3988ec7c58e7c706018cc3d0374a99eb0ba1073
Brad Sickles <brad.sickles@gmail.com>
2021-12-30 14:47:29 -0500

    Fix deployer developer access

--- deployer.tf
+++ deployer.tf
@@ -7,8 +7,8 @@ resource "google_service_account_key" "deployer" {
   service_account_id = google_service_account.deployer.account_id
 }
 
-resource "google_service_account_iam_member" "deployer_developer_access" {
-  service_account_id = google_service_account.deployer.id
-  member             = "serviceAccount:${google_service_account.deployer.email}"
-  role               = "roles/container.developer"
+resource "google_project_iam_binding" "deployer_developer_access" {
+  project = data.google_compute_zones.available.project
+  members = ["serviceAccount:${google_service_account.deployer.email}"]
+  role    = "roles/container.developer"
 }
