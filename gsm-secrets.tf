87cd6e5f613b458962501ee13a559d9b10aab852
Brad Sickles <brad.sickles@gmail.com>
2025-01-29 16:51:37 -0500

    comments

--- gsm-secrets.tf
+++ gsm-secrets.tf
@@ -1,3 +1,5 @@
+// TODO: Migrate to using built-in SecretsManager support from Google
+
 locals {
   cluster_id             = google_container_cluster.primary.id
   cluster_name           = google_container_cluster.primary.name
@@ -15,6 +17,13 @@ provider "helm" {
   }
 }
 
+// We are going to configure the kubernetes cluster with a secrets store
+// This secrets store will provide storage of secrets in google secrets manager instead of kubernetes storage
+// We found two libraries to achieve:
+//   - Secrets Store CSI Driver: https://secrets-store-csi-driver.sigs.k8s.io/
+//   - External Secrets Operator (ESO): https://external-secrets.io/
+// Secrets Store CSI Driver is mentioned by the Google Cloud docs; however, their documentation is poor with limited driver support
+// ESO has extensive documentation, broad secret provider support, and generators (not used yet, but could be useful to teams)
 resource "helm_release" "gsm-external-secrets" {
   name       = "external-secrets"
   repository = "https://charts.external-secrets.io"
